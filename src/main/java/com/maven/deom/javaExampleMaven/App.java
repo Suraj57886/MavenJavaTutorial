3accf745691ef6a5b78e269d8b0b483add1c1b08
Suraj <surajkandagatla101@gmail.com>
2024-11-14 18:49:03 +0530

    Demo for java maven project

---
+++ src/main/java/com/maven/deom/javaExampleMaven/App.java
@@ -0,0 +1,10 @@
+package com.maven.deom.javaExampleMaven;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Suraj 22BD1A052T");
+    }
+}
